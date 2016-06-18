0a5c1192460facb4613e769ba431aeb957d10cc4
王腾飞 <wtf1943@gmail.com>
2016-06-18 15:19:01 +0800

    remove userLog

--- bootAdmin-console/src/main/java/com/snowstore/bootAdmin/controller/IndexController.java
+++ bootAdmin-console/src/main/java/com/snowstore/bootAdmin/controller/IndexController.java
@@ -3,13 +3,10 @@ package com.snowstore.bootAdmin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.snowstore.log.annotation.UserLog;
-
 @Controller
 public class IndexController {
 
 	@RequestMapping("login")
-	@UserLog(remark="登录页面")
 	public String login() {
 		return "login";
 	}
